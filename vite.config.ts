39c3ba7bebb4a4e6a8772aab32d41d80005d3e98
Vitor Reis <vitordesouzareis@gmail.com>
2024-12-16 15:40:10 -0300

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/DCC207TRAB1/',
-  plugins: [react()],
+  base: '/DCC207TRAB1-2/', 
+  plugins: [react()]
 })
