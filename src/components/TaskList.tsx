8df872e0f0559b4c11f9c73ee692d64b9b89ffcf
BrenoFR <breno.rosado@estudante.ufjf.com.br>
2024-12-16 00:51:12 -0300

    Altera tasks para enviar tempo dinâmico ao clock

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Task from './Task';
 import './TaskList.css';
+import Clock from './Clock';
 
 interface TaskListProps {
   onTaskCountChange: (count: number) => void;
@@ -11,11 +12,20 @@ interface TaskType {
   titulo: string;
   iniciado: boolean;
   completo: boolean;
+  segundos: number,
+  minutos: number,
+  horas: number,
 }
 
 export default function TaskList({ onTaskCountChange }: TaskListProps) {
     const [tasks, setTasks] = useState<TaskType[]>([]);
     const [novoTitulo, setNovoTitulo] = useState('');
+    const [novoSegundo, setNovoSegundo] = useState(0);
+    const [novoMinuto, setNovoMinuto] = useState(0);
+    const [novaHora, setNovaHora] = useState(0);
+    const [sendSeconds, setSendSeconds] = useState(0);
+    const [sendMinutes, setSendMinutes] = useState(0);
+    const [sendHours, setSendHours] = useState(0);
 
     useEffect(() => {
       onTaskCountChange(tasks.length);
@@ -31,7 +41,10 @@ export default function TaskList({ onTaskCountChange }: TaskListProps) {
         id: Date.now(),
         titulo: novoTitulo,
         iniciado: false,
-        completo: false
+        completo: false,
+        segundos: novoSegundo,
+        minutos: novoMinuto,
+        horas: novaHora
       };
       setTasks([...tasks, novaTask]);
       setNovoTitulo('');
@@ -40,13 +53,23 @@ export default function TaskList({ onTaskCountChange }: TaskListProps) {
     // Função para deletar uma tarefa pelo ID
     const deletarTask = (id: number) => {
       setTasks(tasks.filter(task => task.id !== id));
+      setSendHours(0);
+      setSendMinutes(0);
+      setSendSeconds(0);
     }
   
-    const iniciarTask = (id: number) => {
+    const iniciarTask = (id: number, seconds: number,minutes: number, hours: number) => {
       setTasks(tasks.map(task => 
         task.id === id ? { ...task, iniciado: true } : task
       ));
+      setSendSeconds(seconds);
+      setSendMinutes(minutes);
+      setSendHours(hours);
     }
+
+    useEffect(() => {
+      console.log(sendMinutes);
+    }, [sendMinutes]);
 
     const completarTask = (id: number) => {
         // Primeiro marca como completo e depois remove após um delay
@@ -60,35 +83,65 @@ export default function TaskList({ onTaskCountChange }: TaskListProps) {
       };
   
     return (
-      <div className="task-container">
-        <div className="adicionar-task">
-          <input
-            type="text"
-            value={novoTitulo}
-            onChange={(e) => setNovoTitulo(e.target.value)}
-            placeholder="Novo desafio..."
-            onKeyPress={(e) => {
-              if (e.key === 'Enter' && novoTitulo.trim()) {
-                adicionarTask();
-              }
-            }}
-          />
-          <button onClick={adicionarTask} disabled={!novoTitulo.trim()}>
-    	     Adicionar
-          </button>
-
-        </div>
-        <div className="task-list">
-          {tasks.map(task => (
-            <Task
-              key={task.id}
-              {...task}
-              onDelete={() => deletarTask(task.id)}
-              onStart={() => iniciarTask(task.id)}
-              onComplete={() => completarTask(task.id)}
+      <div className="general-container">
+        <div className="task-container">
+          <div className="adicionar-task">
+            <input
+              type="text"
+              value={novoTitulo}
+              onChange={(e) => setNovoTitulo(e.target.value)}
+              placeholder="Novo desafio..."
+              onKeyPress={(e) => {
+                if (e.key === 'Enter' && novoTitulo.trim()) {
+                  adicionarTask();
+                }
+              }}
             />
-          ))}
+            <div className='novas-horas'>
+              <p>Horas:⠀</p>
+              <input
+                type="number"
+                className='time'
+                value={novaHora}
+                onChange={(e) => setNovaHora(Number(e.target.value))}
+                placeholder="horas"
+              />
+              <p>⠀Minutos:⠀</p>
+              <input
+                type="number"
+                className='time'
+                value={novoMinuto}
+                onChange={(e) => setNovoMinuto(Number(e.target.value))}
+                placeholder="minutos"
+              />
+              <p>⠀Segundos:⠀</p>
+              <input
+                type="number"
+                className='time'
+                value={novoSegundo}
+                onChange={(e) => setNovoSegundo(Number(e.target.value))}
+                placeholder="segundos"
+              />
+            
+            </div>
+            <button onClick={adicionarTask} disabled={!novoTitulo.trim()}>
+            Adicionar
+            </button>
+
+          </div>
+          <div className="task-list">
+            {tasks.map(task => (
+              <Task
+                key={task.id}
+                {...task}
+                onDelete={() => deletarTask(task.id)}
+                onStart={() => iniciarTask(task.id, task.segundos, task.minutos, task.horas)}
+                onComplete={() => completarTask(task.id)}
+              />
+            ))}
+          </div>
         </div>
+        <Clock reciaveSeconds={sendSeconds} reciaveMinutes={sendMinutes} reciaveHours={sendHours}/>
       </div>
     );
   }
